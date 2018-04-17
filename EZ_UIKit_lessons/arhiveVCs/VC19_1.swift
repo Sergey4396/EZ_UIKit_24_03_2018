04f03bc4cd8a00c84ebc3e87062098b6428077ba
Sergey <s.s.r@mail.ru>
2018-04-17 16:35:35 +0300

    UIKit 19 completed

--- EZ_UIKit_lessons/arhiveVCs/VC19_1.swift
+++ EZ_UIKit_lessons/arhiveVCs/VC19_1.swift
@@ -3,14 +3,37 @@ import UIKit
 
 final class VC19_1: MyVC {
     private var myProgressView : UIProgressView = UIProgressView()
+
+    private var currentProgressView : UIProgressView = UIProgressView()
+    private var allProgressViewArray = [UIProgressView]()
+    private var mainImageView : UIImageView!
+    
     private var myTime = Timer()
-    private var myButton : UIButton!
+    
+    private var currentImageName : String!
+    
+    private let imgNamesArray = ["fry", "Bender", "simpson", "pooh",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        createProgress(myProgressView)
-        myButton = ssButton(title: "button", width: 30, height: 10, x: 50, y: 90)
-        myButton.alpha = 0.2
+        let defaults = UserDefaults.standard
+        print("‼️defaults.integer(forKey: \"ImageNameNomber\") = \(defaults.integer(forKey: "ImageNameNomber"))")
+
+// I don't need if else statement because default value is, not nil
+            currentImageName = imgNamesArray[defaults.integer(forKey: "ImageNameNomber")]
         
+        createProgress(myProgressView)
+        
+//        let _ = createHorizontalProgress(y: h80)
+//        let _ = createHorizontalProgress(y: h85)
+        createAllHorizontalProgress()
+
+        
+                createTimer()
+        mainImageView = ssImageView(image: currentImageName, y: 30, x: 50, width: 90, height: 40, tag: 0)
+        mainImageView.alpha = 0
+        
+//       print(VC19_2.xx)
         
     }
     
@@ -24,32 +47,79 @@ final class VC19_1: MyVC {
     //Mark: selector
     @objc func updateTimer(){
         if myProgressView.progress != 1.0{
-            myProgressView.progress += 0.1
+            myProgressView.progress += 0.2
+            self.mainImageView.alpha = CGFloat(myProgressView.progress)
+            
+            for x in allProgressViewArray{
+                x.progress += 0.2
+            }
         }
         else if myProgressView.progress == 1.0{
-            UIView.animate(withDuration: 0.7, animations: {
-                self.myButton.alpha = 1
-                self.myButton.setTitle("Старт", for: .normal)
-                self.myTime.invalidate()
-            })
+//            UIView.animate(withDuration: 0.7, animations: {
+//                self.myButton.alpha = 1
+//                self.myButton.setTitle("Старт", for: .normal)
+//                self.myTime.invalidate()
+//            })
+            self.myTime.invalidate()
+            
+            goToVC(VC: VC19_2())
         }
         
     }
     //Mark: selector
+        
     private func createProgress(_ progressView: UIProgressView){
         
         progressView.progressViewStyle = .bar
-        progressView.frame = CGRect(x: view.center.x, y: view.center.y, width: 150, height: 50)
+        progressView.frame = CGRect(x: view.center.x, y: h10, width: w90, height: 10)
         progressView.setProgress(0.0, animated: false)
         progressView.progressTintColor = .green
         progressView.trackTintColor = .gray
-        progressView.center = view.center
+        progressView.center = CGPoint(x: w50, y: h10)
         view.addSubview(progressView)
         
     }
     
-    internal override func ffButton(sender: UIButton) {
-        createTimer()
+    private func createHorizontalProgress(y: CGFloat) -> UIProgressView{
+        
+        currentProgressView = UIProgressView()
+        currentProgressView.progressViewStyle = .bar
+        currentProgressView.frame = CGRect(x: view.center.x, y: h10, width: w90, height: 10)
+        currentProgressView.setProgress(0.0, animated: false)
+        currentProgressView.progressTintColor = .green
+        currentProgressView.trackTintColor = .clear
+        currentProgressView.alpha = 0.8
+        currentProgressView.center = CGPoint(x: w50, y: y)
+        view.addSubview(currentProgressView)
+        allProgressViewArray.append(currentProgressView)
+        return currentProgressView
+        
+    }
+    private func createVerticalProgress(x: CGFloat) -> UIProgressView{
+        
+        currentProgressView = UIProgressView()
+        currentProgressView.progressViewStyle = .bar
+        currentProgressView.frame = CGRect(x: view.center.x, y: h10, width: w * 0.62, height: 10)
+        currentProgressView.setProgress(0.0, animated: false)
+        currentProgressView.progressTintColor = .red
+        currentProgressView.trackTintColor = .clear
+        currentProgressView.alpha = 0.5
+        currentProgressView.center = CGPoint(x: x, y: h * 0.775)
+        currentProgressView.transform = CGAffineTransform(rotationAngle: -CGFloat((Double.pi / 2)))
+        view.addSubview(currentProgressView)
+        allProgressViewArray.append(currentProgressView)
+        return currentProgressView
+
+        
+    }
+    private func createAllHorizontalProgress(){
+        
+        for i in 60...95{
+          let _ = createHorizontalProgress(y: h * CGFloat(i) / 100)
+        }
+        for i in 3...48{
+        let _ = createVerticalProgress(x: w * CGFloat(i) / 50 - (w / 200))
+        }
     }
     
 }
--- EZ_UIKit_lessons/arhiveVCs/VC19_2.swift
+++ EZ_UIKit_lessons/arhiveVCs/VC19_2.swift
@@ -1,35 +1,82 @@
-//
-//  VC19_2.swift
-//  EZ_UIKit_lessons
-//
-//  Created by 1234 on 16.04.2018.
-//  Copyright © 2018 Sergey Riabinin. All rights reserved.
-//
 
 import UIKit
 
-class VC19_2: MyVC {
+final class VC19_2: MyVC {
+    
+   private var imgV0 = UIImageView()
+   private var imgV1 = UIImageView()
+   private var imgV2 = UIImageView()
+   private var imgV3 = UIImageView()
 
+//    static var xx : Int = 777
+   private let imgNamesArray = ["fry", "Bender", "simpson", "pooh",]
+    
+
+    private var tapGesture0 = UITapGestureRecognizer()
+    private var tapGesture1 = UITapGestureRecognizer()
+    private var tapGesture2 = UITapGestureRecognizer()
+    private var tapGesture3 = UITapGestureRecognizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+
+        
+        tapGesture0 = UITapGestureRecognizer(target: self, action: #selector(myviewTapped(_:)))
+        tapGesture0.numberOfTapsRequired = 1
+        tapGesture0.numberOfTouchesRequired = 1
+        
+        tapGesture1 = UITapGestureRecognizer(target: self, action: #selector(myviewTapped(_:)))
+        tapGesture1.numberOfTapsRequired = 1
+        tapGesture1.numberOfTouchesRequired = 1
+
+        tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(myviewTapped(_:)))
+        tapGesture2.numberOfTapsRequired = 1
+        tapGesture2.numberOfTouchesRequired = 1
+        
+        tapGesture3 = UITapGestureRecognizer(target: self, action: #selector(myviewTapped(_:)))
+        tapGesture3.numberOfTapsRequired = 1
+        tapGesture3.numberOfTouchesRequired = 1
+
+        
+        imgV0 = ssImageView(image: imgNamesArray[0], y: 25, x: 25, width: 40, height: 30, tag: 0)
+        imgV1 = ssImageView(image: imgNamesArray[1], y: 25, x: 75, width: 40, height: 30, tag: 1)
+        imgV2 = ssImageView(image: imgNamesArray[2], y: 75, x: 25, width: 40, height: 30, tag: 2)
+        imgV3 = ssImageView(image: imgNamesArray[3], y: 75, x: 75, width: 40, height: 30, tag: 3)
+
+        
+        imgV0.addGestureRecognizer(tapGesture0)
+        imgV1.addGestureRecognizer(tapGesture1)
+        imgV2.addGestureRecognizer(tapGesture2)
+        imgV3.addGestureRecognizer(tapGesture3)
+        
+        imgV0.isUserInteractionEnabled = true
+        imgV1.isUserInteractionEnabled = true
+        imgV2.isUserInteractionEnabled = true
+        imgV3.isUserInteractionEnabled = true
+        
+//        imgV0.center = CGPoint(x: -500, y: -500)
+        
+        
+      let defaults = UserDefaults.standard
+        
+        
+        
+        print(defaults.integer(forKey: "Age"))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    @objc func myviewTapped(_ sender: UITapGestureRecognizer){
+        print("myviewTapped")
+print("(sender.view?.tag)! = \((sender.view?.tag)!)")
+       let defaults = UserDefaults.standard
+        defaults.set((sender.view?.tag)!, forKey: "ImageNameNomber")
+        print("‼️🅰️defaults.integer(forKey: \"ImageNameNomber\") = \( defaults.integer(forKey: "ImageNameNomber"))")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        goToVC(VC: VC19_1())
+        
     }
-    */
 
 }
